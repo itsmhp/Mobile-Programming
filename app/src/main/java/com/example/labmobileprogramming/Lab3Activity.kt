8d0e5c7c9e8de543699d30cc4bc90fdb3df7c8e2
Hanif Pratama <mhp.hanif5@gmail.com>
2020-10-03 14:47:42 +0700

    create stopwatch layout

---
+++ app/src/main/java/com/example/labmobileprogramming/Lab3Activity.kt
@@ -0,0 +1,11 @@
+package com.example.labmobileprogramming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lab3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab3)
+    }
+}
